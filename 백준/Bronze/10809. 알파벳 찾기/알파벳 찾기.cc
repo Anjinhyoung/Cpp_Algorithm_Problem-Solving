3ad8a82ff9ed3972d779728f6e5346500a60e73d
AnJinHyoung <117788976+Anjinhyoung@users.noreply.github.com>
2025-01-25 13:55:23 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
@@ -7,18 +7,15 @@ int main() {
     std::ios::sync_with_stdio(false);
 
     std::string str;
+    char alpha[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
     std::cin >> str;
 
-    char array[] = {
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-        'u', 'v', 'w', 'x', 'y', 'z'
-    };
-
+    int num;
     for (int i = 0; i < 26; i++) 
     {
-        int num;
-        num = str.find(array[i]);
-        std::cout << num << ' ';
+        
+        std::cout << (int)str.find(alpha[i]) << " ";
     }
 }
