05a4798c6059ed838e07fc86e6ab1f28eae1a409
AnJinHyoung <117788976+Anjinhyoung@users.noreply.github.com>
2025-03-13 10:31:30 +0900

    [Silver V] Title: 소트인사이드, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.cc
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.cc
@@ -3,15 +3,12 @@
 #include <algorithm>
 
 int main() {
+
     std::string str;
     std::cin >> str;
-
 
     std::sort(str.begin(), str.end());
     std::reverse(str.begin(), str.end());
 
-    for (int i = 0; i < str.size(); i++) 
-    {
-        std::cout << str[i];
-    }
+    std::cout << str;
 }
