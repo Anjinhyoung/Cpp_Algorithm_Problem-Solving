ca28dd01ff984a0ee821db4dda48a3b636da9e45
AnJinHyoung <117788976+Anjinhyoung@users.noreply.github.com>
2025-03-21 13:20:04 +0900

    [Silver III] Title: 칸토어 집합, Time: 4 ms, Memory: 2736 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+
+
+void can(std::string& str, int start, int length) {
+    if (length <= 1) return;
+
+    int length2 = length / 3;
+    int mid = start + length2; // 나중에 1빼기
+    
+    for (int i = mid; i < mid + length2; i++)
+    {
+        str[i] = ' ';
+    }
+
+    // 왼쪽 오른쪽을 나눠야 한다.
+
+    // 왼쪽은 맞는 거 같아
+    can(str, start, length2);
+
+    // 오른쪽 오른쪽이 문젠데
+    can(str, mid + length2, length2);
+}
+
+int main() {
+
+    int num;
+    while (std::cin >> num) 
+    {
+        int length = std::pow(3, num);
+        std::string str(length, '-');
+        can(str, 0, length);
+        std::cout << str << "\n";
+    }
+}
