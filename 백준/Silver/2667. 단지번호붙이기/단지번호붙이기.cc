e48baf1ff915cdfe861a30bb529eadea62f8c454
AnJinHyoung <117788976+Anjinhyoung@users.noreply.github.com>
2025-03-26 14:15:06 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -1,28 +1,30 @@
+// DFS 풀기
+
 #include <iostream>
 #include <vector>
 #include <string>
 #include <algorithm>
 
+int num;
 int map[26][26];
 bool visited[26][26];
-
-int rows[4] = { 0,1,0, -1 };
-int columns[4] = { 1,0,-1,0 };
-int num;
 int cnt;
 std::vector<int> vec;
 
 void dfs(int row, int column) 
 {
+    visited[row][column] = true;
+
+    int rows[4] = { 0,1,0,-1 };
+    int columns[4] = { 1,0,-1,0 };
+
     for (int i = 0; i < 4; i++) 
     {
         int nr = row + rows[i];
         int nc = column + columns[i];
-
-        if (nr > num || nr <= 0 || nc > num || nc <= 0) continue;
-        if (visited[nr][nc] == false && map[nr][nc] == 1) 
+        if (nr <= 0 || nr > num || nc <= 0 || nc > num) continue;
+        if (map[nr][nc] == 1 && !visited[nr][nc]) 
         {
-            visited[nr][nc] = true;
             cnt++;
             dfs(nr, nc);
         }
@@ -32,24 +34,18 @@ void dfs(int row, int column)
 int main() {
     std::cin >> num;
     std::string str;
+    for (int i = 1; i <= num; i++)
+    {
+        std::cin >> str;
+        for (int j = 0; j < str.size(); j++)  if (str[j] == '1') map[i][j + 1] = 1;
+    }
 
     for (int i = 1; i <= num; i++) 
     {
-        std::cin >> str;
-        for (int j = 0; j < str.length(); j++)
+        for (int j = 1; j <= num; j++) 
         {
-            if (str[j] == '1') map[i][j+1] = 1;
-            else map[i][j+1] = 0;
-        }
-    }
-
-    for (int i = 1; i <= num; i++)
-    {
-        for (int j = 1; j <= num; j++)
-        {
-            if (map[i][j] == 1 && visited[i][j] == false)
+            if (!visited[i][j] && map[i][j] == 1) 
             {
-                visited[i][j] = true;
                 cnt = 1;
                 dfs(i, j);
                 vec.push_back(cnt);
@@ -57,12 +53,9 @@ int main() {
         }
     }
 
+    std::cout << vec.size() << '\n';
+    
     std::sort(vec.begin(), vec.end());
 
-    std::cout << vec.size() << "\n";
-
-    for (int i = 0; i < vec.size(); i++) 
-    {
-        std::cout << vec[i] << "\n";
-    }
+    for (int i = 0; i < vec.size(); i++) std::cout << vec[i] << '\n';
 }
