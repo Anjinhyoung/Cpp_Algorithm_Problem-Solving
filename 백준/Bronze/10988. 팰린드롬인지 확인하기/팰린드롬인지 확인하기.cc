c587fa387090056359b59c67c398e88303b364d4
AnJinHyoung <117788976+Anjinhyoung@users.noreply.github.com>
2025-03-28 17:50:43 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -4,14 +4,10 @@
 
 int main() {
 
-    std::cin.tie(nullptr);
-    std::ios::sync_with_stdio(false);
-
     std::string str, str2;
     std::cin >> str;
-    str2 = str; // 초기값 저장
+    str2 = str;
 
-    // 바뀐 뒤에도 같은 지 보기
     std::reverse(str.begin(), str.end());
 
     if (str2 == str) std::cout << 1;
