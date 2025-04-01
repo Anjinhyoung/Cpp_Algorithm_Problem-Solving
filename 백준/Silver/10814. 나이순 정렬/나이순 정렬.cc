12329a5f6acfe9d9459d93101125fbdb9d511bef
AnJinHyoung <117788976+Anjinhyoung@users.noreply.github.com>
2025-04-01 18:15:37 +0900

    [Silver V] Title: 나이순 정렬, Time: 40 ms, Memory: 9844 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -1,35 +1,29 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include <algorithm>
 
 int main() {
 
-    std::cin.tie(nullptr);
     std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
 
-    // stable sort라는 함수가 알고리즘 헤더 파일에 있다.
-    // 순서를 지키는 거
-    int num;
-    std::cin >> num;
-
+    int num; std::cin >> num;
+    
     std::vector<std::pair<int, std::string>> vec;
 
     for (int i = 0; i < num; i++) 
     {
-        int a;
-        std::string str;
-        std::cin >> a >> str;
-        vec.push_back({ a, str });
+        int a; std::string b; std::cin >> a >> b;
+
+        vec.push_back({ a,b });
     }
 
-    std::stable_sort(vec.begin(), vec.end(), [](const std::pair<int, std::string>& a, const std::pair<int, std::string>& b)
-        { return a.first < b.first; });
+    std::stable_sort(vec.begin(), vec.end(), [](const auto& a, const auto& b) 
+        {
+            // second는 무시
+            return a.first < b.first;
+        });
 
-    // a.first < b.first => 오름차순
-    // a.first > b.fisrt => 내림차순
-
-    for (int i = 0; i < vec.size(); i++) 
-    {
-        std::cout << vec[i].first << " " << vec[i].second << "\n";
-    }
+    for (int i = 0; i < num; i++)  std::cout << vec[i].first << ' ' << vec[i].second << '\n';
 }
