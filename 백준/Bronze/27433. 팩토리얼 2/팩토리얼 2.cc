d28ffa8b6d979fbfc5b633e9309fd428411a17fa
AnJinHyoung <117788976+Anjinhyoung@users.noreply.github.com>
2025-04-03 18:17:21 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.cc
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.cc
@@ -2,24 +2,15 @@
 
 long long factorial(long long num) 
 {
-    if (num == 1) return num;
+    if (num == 0) return 1;
 
     return num * factorial(num - 1);
 }
 
-
 int main() {
 
-    std::cin.tie(nullptr);
-    std::ios::sync_with_stdio(false);
+    int num; std::cin >> num;
 
-    long long num;
-    std::cin >> num;
-    if (num == 0) 
-    {
-        std::cout << 1;
-        return 0;
-    }
-   
     std::cout << factorial(num);
+
 }
