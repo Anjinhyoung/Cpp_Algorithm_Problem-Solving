b1b51a1539cb72e6f823cd6fee4b4795025352ef
AnJinHyoung <117788976+Anjinhyoung@users.noreply.github.com>
2025-04-21 19:13:01 +0900

    [Silver V] Title: 집합, Time: 568 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+
+int count[21];
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int num; std::cin >> num;
+
+    std::string str; int num2;
+
+    for (int i = 0; i < num; i++)
+    {
+        std::cin >> str;
+
+        if (str == "add")
+        {
+            std::cin >> num2;
+            count[num2] = 1;
+        }
+        else if (str == "remove")
+        {
+            std::cin >> num2;
+            count[num2] = 0;
+        }
+        else if (str == "check")
+        {
+            std::cin >> num2;
+            if (count[num2] == 1) std::cout << 1 << '\n';
+            else std::cout << 0 << '\n';
+        }
+
+        else if (str == "toggle")
+        {
+            std::cin >> num2;
+
+            if (count[num2] == 1) count[num2] = 0;
+            else count[num2] = 1;
+        }
+
+        else if (str == "all") for (int j = 1; j <= 20; j++)  count[j] = 1;
+
+        else for (int j = 1; j <= 20; j++) count[j] = 0;
+    }
+}
